68ea68186ea1dd5819216c7f43eb883079e88c8f
Andrew <21962412+ndrewfrost@users.noreply.github.com>
2020-06-10 10:30:15 +0100

    Scene Graph Sampler

--- vulkan_samples/scene graph/components/sampler.hpp
+++ vulkan_samples/scene graph/components/sampler.hpp
@@ -1,25 +1,38 @@
 /*
  *
  * Andrew Frost
- * .hpp
+ * sampler.hpp
  * 2020
  *
  */
 
 #pragma once
 
+#include <memory>
+#include <vulkan/vulkan.hpp>
 #include "../component.hpp"
 
 namespace vkb {
 namespace sg {
 
 ///////////////////////////////////////////////////////////////////////////
-//                                                                //
+// Sampler                                                               //
 ///////////////////////////////////////////////////////////////////////////
 
-class : public Component
+class Sampler : public Component
 {
+public:
+    Sampler(const std::string& name, vk::Sampler sampler)
+        : Component(name) { m_sampler = sampler; }
 
+    Sampler(Sampler&& other) = default;
+
+    virtual ~Sampler() = default;
+
+    virtual std::type_index getType() override { return typeid(Sampler); }
+
+public:
+    vk::Sampler m_sampler;
 };
 
 } // namespace sg
