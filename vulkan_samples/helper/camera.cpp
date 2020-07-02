2b00ea5f923a45efb684ff31705bc285268e24ec
Andrew <21962412+ndrewfrost@users.noreply.github.com>
2020-07-02 10:13:06 +0100

    camera structure, no transformation operations

--- vulkan_samples/helper/camera.cpp
+++ vulkan_samples/helper/camera.cpp
@@ -7,3 +7,83 @@
  */
 
 #include "camera.hpp"
+
+namespace tools {
+
+//-------------------------------------------------------------------------
+// Math Functions
+//
+template <typename T>
+bool isZero(const T& _a)
+{
+    return fabs(_a) < std::numeric_limits<T>::epsilon();
+}
+
+template <typename T>
+bool isOne(const T& _a)
+{
+    return areEqual(_a, (T)1);
+}
+
+inline float sign(float s)
+{
+    return (s < 0.f) ? -1.f : 1.f;
+}
+
+///////////////////////////////////////////////////////////////////////////
+// Camera                                                                //
+///////////////////////////////////////////////////////////////////////////
+
+//-------------------------------------------------------------------------
+// Constructor
+//
+Camera::Camera()
+{
+    update();
+}
+
+//-------------------------------------------------------------------------
+// update internal transformation matrix
+//
+void Camera::update()
+{
+    m_matrix = glm::lookAt(m_pos, m_int, m_up);
+
+    if (!isZero(m_roll)) {
+        glm::mat4 rotate = glm::rotate(m_roll, glm::vec3(0, 0, 1));
+        m_matrix = m_matrix * rotate;
+    }
+}
+
+//-------------------------------------------------------------------------
+// Set camera information and derive viewing matrix
+//
+void Camera::setLookAt(glm::vec3& eye, glm::vec3& center, glm::vec3& up)
+{
+    m_pos = eye;
+    m_int = center;
+    m_up  = up;
+    update();
+}
+
+//--------------------------------------------------------------------------------------------------
+// Retreive current camera information
+// Position, interest and up vector
+//
+void Camera::getLookAt(glm::vec3& eye, glm::vec3& center, glm::vec3& up) const
+{
+    eye    = m_pos;
+    center = m_int;
+    up     = m_up;
+}
+
+//-------------------------------------------------------------------------
+// Retreive transformation matrix of camera
+//
+const glm::mat4& Camera::getMatrix() const
+{
+    return m_matrix;
+}
+
+
+} // ! namespace tools
--- vulkan_samples/helper/camera.hpp
+++ vulkan_samples/helper/camera.hpp
@@ -8,8 +8,8 @@
 
 #pragma once
 
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/quaternion.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 
 namespace tools {
 
@@ -19,7 +19,31 @@ namespace tools {
 
 class Camera
 {
-    
+public:
+    Camera();
+
+    ~Camera() = default;
+
+    void update();
+
+    void setLookAt(glm::vec3& eye, glm::vec3& center, glm::vec3& up);
+
+    void getLookAt(glm::vec3& eye, glm::vec3& center, glm::vec3& up) const;
+
+    const glm::mat4& getMatrix() const;
+
+private:
+    // Camera Position
+    glm::vec3 m_pos    = glm::vec3(1.f, 1.f, 1.f);
+    glm::vec3 m_int    = glm::vec3(0.f, 0.f, 0.f);
+    glm::vec3 m_up     = glm::vec3(0.f, 1.f, 0.f);
+    glm::mat4 m_matrix = glm::mat4(1.f);
+    float     m_roll   = 0.f; // Rotation around Z axis
+
+    // Screen 
+    int m_width  = 1;
+    int m_height = 1;
+
 }; // ! class Camera
 
 } // ! namespace tools
